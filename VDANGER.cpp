a213ce51b7bfd4c914d93796823f7a44e0685eaf
itskoi <kelvin.asvn@gmail.com>
2017-12-27 18:16:22 +0700

    Update VDANGER.cpp

--- VDANGER.cpp
+++ VDANGER.cpp
@@ -12,11 +12,12 @@ vector<int> path;
 int n, m;
 
 void Init() {
-    memset(adj, 0x3f, sizeof(adj));
+    //memset(adj, 0x3f, sizeof(adj));
 }
 
 void Input() {
-    cin >> n >> m;
+    //freopen("VDANGER.INP", "r", stdin);
+    cin >> n >> m; path.assign(m + 1, 0);
     rep(i, 1, m) cin >> path[i];
     rep(i, 1, n) rep(j, 1, n) cin >> adj[i][j];
 }
@@ -26,8 +27,12 @@ void minimize(int &a, int b) {
 }
 
 void Compute() {
+    int ans = 0;
     rep(k, 1, n) rep(i, 1, n) rep(j, 1, n) minimize(adj[i][j], adj[i][k] + adj[k][j]);
-
+    if (path[1] != 1) ans += adj[1][path[1]];
+    rep(i, 1, m - 1) ans += adj[path[i]][path[i + 1]];
+    if (path[m] != n) ans += adj[path[m]][n];
+    cout << ans;
 }
 
 int main() {
